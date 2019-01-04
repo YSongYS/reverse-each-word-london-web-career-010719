81b6ecc80394b0bd5fff0a90bf2301bbe96af1e5
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 22:51:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(' ')
   new_string = ""
   array.each { |element| element << element.reverse }
-  new_string = array,join(' ')
+  new_string = array.join(' ')
   #return new_string
   #new_string = array.join('')
 end
